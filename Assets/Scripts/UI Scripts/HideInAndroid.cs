dcc4973eb44239a20c23575c2437ee925019677d
Taurustar <scarreno@dark-ware.com>
2021-04-11 05:02:47 -0400

    Hidden UI Controls in PC

---
+++ Assets/Scripts/UI Scripts/HideInAndroid.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HideInAndroid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+#if UNITY_STANDALONE
+        gameObject.GetComponent<Image>().enabled = false;
+#endif
+    }
+}
