0c9a76d1a0c839e3b73d1635cd33fcfa7e223251
Taurustar <scarreno@dark-ware.com>
2021-04-11 04:43:23 -0400

    Menu instructions for Android added Cleaned some assets from the project

---
+++ Assets/Scripts/UI Scripts/InstructionsByPlatform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionsByPlatform : MonoBehaviour
+{
+    public Canvas pcInstructions, androidInstructions;
+
+    public void Action()
+    {
+#if UNITY_STANDALONE
+        pcInstructions.enabled = true;
+#endif
+
+#if UNITY_ANDROID
+        androidInstructions.enabled = true;
+#endif
+    }
+}
